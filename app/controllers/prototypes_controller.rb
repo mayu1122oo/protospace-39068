3d78012211e96e6f5b29a243e6fab88dc730ee67
mayu1122oo <mayu.0310pcq123@gmail.com>
2023-03-15 15:08:54 +0900

    編集ページのコード修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -37,6 +37,7 @@ class PrototypesController < ApplicationController
     if prototype.update(prototype_params)
       redirect_to prototype_path
     else
+      @prototype = Prototype.update(prototype_params)
       render :edit
     end
   end
